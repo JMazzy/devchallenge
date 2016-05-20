d425a59208aa92e99c07b7b27d9756531b16af34
Joshua Masland-Sarani <j.masland@gmail.com>
2016-05-19 20:50:40 -0400

    user auth

--- db/migrate/20160520000523_create_users.rb
+++ db/migrate/20160520000523_create_users.rb
@@ -1,10 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :email, unique: true, null: false
+      t.string :email, null: false
       t.string :password_digest
       t.string :auth_token
 
+      t.index :email, unique: true
       t.index :auth_token, unique: true
 
       t.timestamps null: false
