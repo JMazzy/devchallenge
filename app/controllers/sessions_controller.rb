4bdaf01e1180e6b078daf7550b27660ee4bef297
Joshua Masland-Sarani <j.masland@gmail.com>
2016-05-20 09:54:42 -0400

    work on sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,7 +19,7 @@ class SessionsController < ApplicationController
       flash[:success] = "You've successfully signed in"
       redirect_to root_url
     else
-      flash.now[:error] = "We couldn't sign you in"
+      flash.now[:danger] = "We couldn't sign you in"
       render :new
     end
   end
